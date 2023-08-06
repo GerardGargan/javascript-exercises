a81a3885c55ca76f3ef1b6760c7f615c3b073b97
Gerard Gargan <gerardgargan@hotmail.co.uk>
2023-08-06 22:20:00 +0100

    completed exercise 4

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,6 +1,21 @@
-const removeFromArray = function() {
+const removeFromArray = function(array, ...toRemove) {
 
-};
+    if(toRemove==="" || toRemove===null || toRemove ===undefined){
+        return array;
+    } else {
+    
+        const newArray = [];
+
+        for(value of array){
+            if(!toRemove.includes(value)){
+                newArray.push(value);
+            }
+        }
+
+        return newArray;
+    }
+        
+    };
 
 // Do not edit below this line
 module.exports = removeFromArray;
