ed5fa26c24ecbdcab882c2b15446992e634e08d4
Gerard Gargan <gerardgargan@hotmail.co.uk>
2023-08-06 21:55:00 +0100

    redone exercise 3 using recursive function

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,11 +1,17 @@
 const reverseString = function(string) {
     let reversed = "";
 
-    for(let i = string.length-1; i>=0; i--){
-        reversed += string[i];
-    }
+    // for(let i = string.length-1; i>=0; i--){
+    //     reversed += string[i];
+    // }
 
-    return reversed;
+    // return reversed;
+
+    if(string.length<=1){
+        return string;
+    } else {
+        return reverseString(string.substring(1))+string.charAt(0);
+    }
 
 };
 
