fb5e4b765d9d5a9b0c711b33d78e9d7224886cc9
Gerard Gargan <gerardgargan@hotmail.co.uk>
2023-08-08 19:57:13 +0100

    completed exercise 5

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,22 @@
-const sumAll = function() {
+const sumAll = function(num1, num2) {
+    let total = 0;
 
+    if(num1<0 || num2<0){
+        return "ERROR";
+    }
+
+    if(!Number.isInteger(num1) || !Number.isInteger(num2)){
+        return "ERROR";
+    }
+    
+    const biggest = (num1>num2) ? num1 : num2;
+    const smallest = (num1<num2) ? num1 : num2;
+
+    for(let i=smallest; i<=biggest; i++){
+        total += i;
+    }
+
+return total;
 };
 
 // Do not edit below this line
